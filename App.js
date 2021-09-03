e22deb1e6d1e95c88edf1bec68d03f5b2743b643
Lucas Zanganelli <lucasfzanganelli@gmail.com>
2021-09-02 22:20:35 -0300

    Criacao do componente
    Criei o componente caixa de texto

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Slider from '@react-native-community/slider';
 import { StyleSheet, Text, View, TextInput} from 'react-native';
+import Cxtx from './components/CaixadeTexto';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Opeeeeen up Aaaaaaaaaaaaaaaaaaaaaaaapp.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Cxtx/>
     </View>
   );
 }
---
+++ components/CaixadeTexto.js
@@ -0,0 +1,14 @@
+import React from React;
+import {View, TextInput, Text} from 'react-native';
+
+export default function (){
+    return(
+
+        <View>
+        
+          <Text>Digite seu nome:</Text>
+          <TextInput/>
+
+        </View>
+    );
+}
